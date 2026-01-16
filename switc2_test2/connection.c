f306ccd5f27b81b440a2830bf3122f1a660ca9d4
270386y <270386f@ugs.kochi-tech.ac.jp>
2026-01-16 14:59:32 +0000

    大富豪狙うやつ

---
+++ switc2_test2/connection.c
@@ -0,0 +1,350 @@
+/*connection*/
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "connection.h"
+#include "daihinmin.h"
+
+#define PROTOCOL_VERSION 20070     //?ｿｽv?ｿｽ?ｿｽ?ｿｽg?ｿｽR?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ@?ｿｽ[?ｿｽW?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ\?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ
+#define DEFAULT_SERVER "127.0.0.1" //?ｿｽf?ｿｽt?ｿｽH?ｿｽ?ｿｽ?ｿｽg?ｿｽﾌサ?ｿｽ[?ｿｽo?ｿｽﾌア?ｿｽh?ｿｽ?ｿｽ?ｿｽX ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾅ指?ｿｽ?ｿｽ
+#define DEFAULT_PORT 42485         //?ｿｽf?ｿｽt?ｿｽH?ｿｽ?ｿｽ?ｿｽg?ｿｽﾌポ?ｿｽ[?ｿｽg?ｿｽﾔ搾ｿｽ ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾅ指?ｿｽ?ｿｽ
+#define DEFAULT_NAME "test2"     //?ｿｽf?ｿｽt?ｿｽH?ｿｽ?ｿｽ?ｿｽg?ｿｽﾌク?ｿｽ?ｿｽ?ｿｽC?ｿｽA?ｿｽ?ｿｽ?ｿｽg?ｿｽ?ｿｽ ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾅ指?ｿｽ?ｿｽ
+
+#define SOCKET_PATH "23805"
+
+extern const int g_logging;
+
+extern struct state_type state;
+extern struct seatsort_state ss;
+
+/* ?ｿｽﾃ的?ｿｽﾖ撰ｿｽ?ｿｽﾌ関撰ｿｽ?ｿｽv?ｿｽ?ｿｽ?ｿｽg?ｿｽ^?ｿｽC?ｿｽv?ｿｽ骭ｾ */
+static int refreshTable(int table_val[8][15]);
+static int sendTable(int table_val[8][15]);
+static int openSocket(const char ip_addr[], uint16_t portnum_data);
+static int sendProfile(const char user[15]);
+
+/*connection?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾏ撰ｿｽ*/
+
+//?ｿｽ\?ｿｽP?ｿｽb?ｿｽg?ｿｽﾖ連?ｿｽﾌ変撰ｿｽ?ｿｽ?ｿｽﾃ的?ｿｽO?ｿｽ?ｿｽ?ｿｽ[?ｿｽo?ｿｽ?ｿｽ?ｿｽﾏ撰ｿｽ?ｿｽﾆゑｿｽ?ｿｽﾄ宣言
+static int g_sockfd;
+static int g_buf_len;
+static struct sockaddr_in g_client_addr;
+
+//?ｿｽﾚ托ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽT?ｿｽ[?ｿｽo?ｿｽA?ｿｽ|?ｿｽ[?ｿｽg?ｿｽ?ｿｽ?ｿｽi?ｿｽ[?ｿｽ?ｿｽ?ｿｽ?ｿｽﾏ撰ｿｽ
+static char server_name[256] = DEFAULT_SERVER;
+static uint16_t port = DEFAULT_PORT;
+//?ｿｽT?ｿｽ[?ｿｽo?ｿｽﾉ通知?ｿｽ?ｿｽ?ｿｽ?ｿｽN?ｿｽ?ｿｽ?ｿｽC?ｿｽA?ｿｽ?ｿｽ?ｿｽg?ｿｽ?ｿｽ
+static char user_name[15] = DEFAULT_NAME;
+
+//?ｿｽe?ｿｽ[?ｿｽu?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽM?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?狽?ｿｽ?ｿｽJ?ｿｽE?ｿｽ?ｿｽ?ｿｽg
+static int table_count = 0;
+
+//host?ｿｽﾉ接托ｿｽ?ｿｽ?ｿｽ ?ｿｽQ?ｿｽ[?ｿｽ?ｿｽ?ｿｽﾉ参?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ ?ｿｽv?ｿｽ?ｿｽ?ｿｽ[?ｿｽ?ｿｽ?ｿｽ[?ｿｽﾔ搾ｿｽ?ｿｽ?ｿｽﾔゑｿｽ
+int entryToGame(void)
+{
+  int my_playernum; //?ｿｽv?ｿｽ?ｿｽ?ｿｽC?ｿｽ?ｿｽ?ｿｽ[?ｿｽﾔ搾ｿｽ?ｿｽ?ｿｽ?ｿｽL?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ
+  //?ｿｽT?ｿｽ[?ｿｽo?ｿｽﾉ対ゑｿｽ?ｿｽﾄソ?ｿｽP?ｿｽb?ｿｽg?ｿｽ?ｿｽp?ｿｽﾓゑｿｽ?ｿｽAconnect?ｿｽ?ｿｽ?ｿｽ?ｿｽ
+  if ((openSocket(server_name, port)) == -1) {
+    printf("failed to open socket to server[%s:%d].\n", server_name, port);
+    exit(1);
+  }
+  if (g_logging == 1) {
+    printf("connectiong to server was finished successfully[%s:%d].\n", server_name, port);
+  }
+
+  sendProfile(user_name); //?ｿｽN?ｿｽ?ｿｽ?ｿｽC?ｿｽA?ｿｽ?ｿｽ?ｿｽg?ｿｽﾌ擾ｿｽ?ｿｽ?乱M
+
+  if (g_logging == 1) {
+    printf("send profile .\n");
+  }
+  //?ｿｽ?ｿｽ?ｿｽg?ｿｽﾌプ?ｿｽ?ｿｽ?ｿｽC?ｿｽ?ｿｽ?ｿｽ[?ｿｽﾔ搾ｿｽ?ｿｽ?ｿｽ?ｿｽT?ｿｽ[?ｿｽo?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ轤､
+  if (read(g_sockfd, &my_playernum, sizeof(my_playernum)) > 0) {
+    my_playernum = ntohl(my_playernum);
+    if (g_logging == 1) {
+      printf("my player number is %d\n", my_playernum);
+    }
+  } else {
+    printf("failed to get player number.\n");
+    exit(1);
+  }
+
+  return my_playernum; //?ｿｽ?ｿｽ?ｿｽg?ｿｽﾌプ?ｿｽ?ｿｽ?ｿｽC?ｿｽ?ｿｽ?ｿｽ[?ｿｽﾔ搾ｿｽ?ｿｽ?ｿｽﾔゑｿｽ?ｿｽB
+}
+
+//?ｿｽ\?ｿｽP?ｿｽb?ｿｽg?ｿｽ?ｿｽclose?ｿｽ?ｿｽ?ｿｽs?ｿｽ?ｿｽ ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ鼾??ｿｽ?ｿｽ 0 ?ｿｽ?ｿｽﾔゑｿｽ?ｿｽB?ｿｽG?ｿｽ?ｿｽ?ｿｽ[?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ鼾??ｿｽ?ｿｽ -1 ?ｿｽ?ｿｽﾔゑｿｽ
+int closeSocket()
+{
+  return close(g_sockfd);
+}
+
+//?ｿｽV?ｿｽ?ｿｽ?ｿｽb?ｿｽt?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ黷ｽ?ｿｽJ?ｿｽ[?ｿｽh?ｿｽ?ｿｽ?ｿｽ?ｯ趣ｿｽ?ｿｽ ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾚのゲ?ｿｽ[?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾔゑｿｽ
+int startGame(int table[8][15])
+{
+  static int game_count = 0;
+  //?ｿｽ?ｿｽ?ｿｽO?ｿｽ?ｿｽ閧ｪ?ｿｽL?ｿｽ?ｿｽ?ｿｽﾈゑｿｽQ?ｿｽ[?ｿｽ?ｿｽ?ｿｽﾔ搾ｿｽ?ｿｽ?ｿｽ\?ｿｽ?ｿｽ
+  if (g_logging == 1) {
+    printf("game number is %d.\n", game_count);
+  }
+  table_count = 0; // ?ｿｽQ?ｿｽ[?ｿｽ?ｿｽ?ｿｽJ?ｿｽn?ｿｽﾆ難ｿｽ?ｿｽ?ｿｽ?ｿｽﾉテ?ｿｽ[?ｿｽu?ｿｽ?ｿｽ?ｿｽﾌカ?ｿｽE?ｿｽ?ｿｽ?ｿｽg?ｿｽ?ｿｽ0?ｿｽﾉゑｿｽ?ｿｽ?ｿｽB
+  //?ｿｽ?ｿｽ?ｿｽ?ｿｽﾌカ?ｿｽ[?ｿｽh?ｿｽ?ｿｽ?ｿｽ?ｯ趣ｿｽ?ｿｽ
+  if (((refreshTable(table))) == -1) {
+    printf("failed to get initial cards table for exchange.\n");
+    exit(1);
+  }
+
+  //?ｿｽ?ｿｽ?ｿｽO?ｿｽ?ｿｽ閧ｪ?ｿｽL?ｿｽ?ｿｽ?ｿｽﾈゑｿｽA?ｿｽz?ｿｽz?ｿｽ?ｿｽ?ｿｽ黷ｽ?ｿｽJ?ｿｽ[?ｿｽh(?ｿｽﾆゑｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽe?ｿｽ[?ｿｽu?ｿｽ?ｿｽ)?ｿｽ?ｿｽ\?ｿｽ?ｿｽ
+  if (g_logging == 1) {
+    printf("initial cards table is follow.\n");
+    outputTable(table);
+  }
+
+  game_count++;
+
+  return game_count;
+}
+
+//?ｿｽJ?ｿｽ[?ｿｽh?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾌカ?ｿｽ[?ｿｽh?ｿｽﾌ抵ｿｽo
+void sendChangingCards(int cards[8][15])
+{
+  if (sendTable(cards) == -1) {
+    //?ｿｽ?ｿｽ?ｿｽs?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽG?ｿｽ?ｿｽ?ｿｽ[?ｿｽ?ｿｽ?ｿｽﾍゑｿｽ?ｿｽﾄ抵ｿｽ~
+    printf("sending cards table was failed.\n");
+    exit(1);
+  } else {
+    //?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽloggin?ｿｽt?ｿｽ?ｿｽ?ｿｽO?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾄゑｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽA?ｿｽ?ｿｽ?ｿｽb?ｿｽZ?ｿｽ[?ｿｽW?ｿｽ?ｿｽf?ｿｽ?ｿｽ
+    if (g_logging == 1) {
+      printf("sending cards-table was done successfully.\n");
+    }
+  }
+  //loggin?ｿｽt?ｿｽ?ｿｽ?ｿｽO?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾄゑｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽA?ｿｽe?ｿｽ[?ｿｽu?ｿｽ?ｿｽ?ｿｽﾌ難ｿｽ?ｿｽe?ｿｽ?ｿｽ?ｿｽo?ｿｽ?ｿｽ
+  if (g_logging == 1) {
+    //?ｿｽe?ｿｽ[?ｿｽu?ｿｽ?ｿｽ?ｿｽﾌ難ｿｽ?ｿｽe?ｿｽ?ｿｽ?ｿｽo?ｿｽ?ｿｽ
+    printf("sent card table was......\n");
+    outputTable(cards);
+  }
+}
+
+//?ｿｽJ?ｿｽ[?ｿｽh?ｿｽ?ｿｽ?ｿｽ?ｯ趣ｿｽ?ｿｽ ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾌタ?ｿｽ[?ｿｽ?ｿｽ?ｿｽﾅゑｿｽ?ｿｽ?ｿｽ?ｿｽ1?ｿｽ?ｿｽﾔゑｿｽ
+int receiveCards(int cards[8][15])
+{
+  //?ｿｽJ?ｿｽ[?ｿｽh?ｿｽ?ｿｽ?ｿｽ?ｯ趣ｿｽ?ｿｽ
+  if (((refreshTable(cards))) == -1) {
+    //?ｿｽ?ｿｽ?ｿｽs?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ鼾??ｿｽﾌエ?ｿｽ?ｿｽ?ｿｽ[?ｿｽ?ｿｽ?ｿｽ?ｿｽ
+    printf("failed to get my cards table.\n");
+    exit(1);
+  } else {
+    //?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ logging?ｿｽt?ｿｽ?ｿｽ?ｿｽO?ｿｽﾉ会ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽO?ｿｽﾌ表?ｿｽ?ｿｽ
+    if (g_logging == 1) {
+      printf("recieved my cards table.\n");
+      printf("  table count=%d\n", table_count);
+      outputTable(cards);
+    }
+  }
+  getState(cards); //?ｿｽ?ｿｽﾌ擾ｿｽﾔの読み出?ｿｽ?ｿｽ
+  return cards[5][2];
+}
+
+//?ｿｽJ?ｿｽ[?ｿｽh?ｿｽ?ｿｽ?ｿｽo?ｿｽ?ｿｽ?ｿｽ?揩?ｿｽ?ｿｽ黷ｽ?ｿｽ?ｿｽ?ｿｽﾛゑｿｽ?ｿｽ?ｿｽﾔゑｿｽ
+int sendCards(int cards[8][15])
+{
+  int accept_flag; //?ｿｽ?ｿｽo?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽJ?ｿｽ[?ｿｽh?ｿｽ?ｿｽ?ｿｽ?揩?ｿｽ?ｿｽ黷ｽ?ｿｽ?ｿｽ?ｿｽ?ｻ別ゑｿｽ?ｿｽ?ｿｽﾏ撰ｿｽ
+  //?ｿｽJ?ｿｽ[?ｿｽh?ｿｽ?乱M?ｿｽ?ｿｽ?ｿｽ?ｿｽ
+  if (g_logging == 1) {
+    printf("send card table was following.\n");
+    outputTable(cards);
+  }
+
+  if ((sendTable(cards)) == -1) { //?ｿｽJ?ｿｽ[?ｿｽh?ｿｽ?乱M?ｿｽ?ｿｽ ?ｿｽ?ｿｽ?ｿｽs?ｿｽ?ｿｽ?ｿｽﾍ??ｿｽ?ｿｽb?ｿｽZ?ｿｽ[?ｿｽW?ｿｽ?ｿｽ\?ｿｽ?ｿｽ
+    printf("failed to send card sending table. \n");
+    exit(1);
+  }
+
+  //accept_flag?ｿｽ?ｿｽ?ｿｽT?ｿｽ[?ｿｽo?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｯ趣ｿｽ?ｿｽ
+  if ((read(g_sockfd, &accept_flag, sizeof(accept_flag))) < 0) {
+    printf("failed to get accept sign.\n");
+    exit(1);
+  } else {
+    if (g_logging == 1) {
+      printf("accept_flag=%d\n", (int)ntohl(accept_flag));
+    }
+  }
+  return ntohl(accept_flag);
+}
+
+//?ｿｽ?ｿｽ?ｿｽE?ｿｽ?ｿｽ?ｿｽh?ｿｽﾌ最鯉ｿｽﾉゲ?ｿｽ[?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽI?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ ?ｿｽT?ｿｽ[?ｿｽo?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｯとゑｿｽ ?ｿｽ?ｿｽ?ｿｽﾌ値?ｿｽ?ｿｽﾔゑｿｽ?ｿｽB
+int beGameEnd(void)
+{
+  int one_gameend_flag;
+  //?ｿｽQ?ｿｽ[?ｿｽ?ｿｽ?ｿｽI?ｿｽ?ｿｽ?ｿｽﾌフ?ｿｽ?ｿｽ?ｿｽO?ｿｽ?ｿｽ?ｿｽ?ｯ趣ｿｽ?ｿｽ
+  if ((read(g_sockfd, &one_gameend_flag, sizeof(one_gameend_flag))) < 0) {
+    //?ｿｽ?ｿｽM?ｿｽ?ｿｽ?ｿｽs?ｿｽ?ｿｽ ?ｿｽ?ｿｽ?ｿｽb?ｿｽZ?ｿｽ[?ｿｽW?ｿｽ?ｿｽ\?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ~
+    printf("failed to check if the game was finished.\n");
+    exit(1);
+  } else {
+    //?ｿｽ?ｿｽM?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ ?ｿｽl?ｿｽﾌバ?ｿｽC?ｿｽg?ｿｽI?ｿｽ[?ｿｽ_?ｿｽ[?ｿｽ?ｼゑｿｽ
+    one_gameend_flag = ntohl(one_gameend_flag);
+  }
+  return one_gameend_flag;
+}
+
+void lookField(int cards[8][15])
+{
+  if (((refreshTable(cards))) == -1) {
+    //?ｿｽ?ｿｽ?ｿｽs?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ鼾??ｿｽﾌエ?ｿｽ?ｿｽ?ｿｽ[?ｿｽ?ｿｽ?ｿｽ?ｿｽ
+    printf("failed to get result table.\n");
+    exit(1);
+  } else {
+    //?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽO?ｿｽ?ｿｽ\?ｿｽ?ｿｽ
+    if (g_logging == 1) {
+      printf("received result table.\n");
+      outputTable(cards);
+      printf("end bacards\n");
+    }
+  }
+  getField(cards); //?ｿｽ?ｿｽﾉ出?ｿｽﾄゑｿｽ?ｿｽ?ｿｽJ?ｿｽ[?ｿｽh?ｿｽﾌ擾ｿｽ?ｿｽ?ｿｽﾇみ出?ｿｽ?ｿｽ
+}
+
+void checkArg(int argc, char *argv[])
+{
+  /*
+    ?ｿｽn?ｿｽ?ｿｽ?ｿｽ黷ｽ?ｿｽR?ｿｽ}?ｿｽ?ｿｽ?ｿｽh?ｿｽ?ｿｽ?ｿｽC?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ^?ｿｽ?ｿｽ?ｿｽ?ｿｽ黷ｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾌ擾ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾍゑｿｽ?ｿｽA?ｿｽK?ｿｽv?ｿｽﾉ会ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ
+    ?ｿｽT?ｿｽ[?ｿｽo?ｿｽA?ｿｽh?ｿｽ?ｿｽ?ｿｽX?ｿｽA?ｿｽ|?ｿｽ[?ｿｽg?ｿｽﾔ搾ｿｽ?ｿｽA?ｿｽN?ｿｽ?ｿｽ?ｿｽC?ｿｽA?ｿｽ?ｿｽ?ｿｽg?ｿｽ?ｿｽ?ｿｽ?ｿｽﾏ更?ｿｽ?ｿｽ?ｿｽ?ｿｽB
+  */
+  const char Option[] = "[-h server_adress] [-p port] [-n user_name]";
+  int arg_count = 1;
+
+  while (arg_count < argc) {
+    if (strcmp(argv[arg_count], "--help") == 0) {
+      printf("usage : %s %s\n", argv[0], Option);
+      exit(0);
+    } else if (strcmp(argv[arg_count], "-h") == 0) {
+      arg_count++;
+      if (arg_count < argc) {
+        strcpy(server_name, argv[arg_count]);
+      } else {
+        printf("%s -h hostname\n", argv[0]);
+        exit(1);
+      }
+    } else if ((strcmp(argv[arg_count], "-p")) == 0) {
+      arg_count++;
+      if (arg_count < argc) {
+        port = (uint16_t)atoi(argv[arg_count]);
+      } else {
+        printf("%s -p port\n", argv[0]);
+        exit(1);
+      }
+    } else if ((strcmp(argv[arg_count], "-n")) == 0) {
+      arg_count++;
+      if (arg_count < argc) {
+        strcpy(user_name, argv[arg_count]);
+      } else {
+        printf("%s -n user_name\n", argv[0]);
+        exit(1);
+      }
+    } else {
+      printf("%s : unknown option : %s \n", argv[0], argv[arg_count]);
+      printf("usage : %s %s\n", argv[0], Option);
+      exit(1);
+    }
+    arg_count++;
+  }
+}
+
+//?ｿｽT?ｿｽ[?ｿｽo?ｿｽ?ｿｽ?ｿｽ?ｿｽe?ｿｽ[?ｿｽu?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｯ趣ｿｽ?ｿｽA?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾈゑｿｽ0?ｿｽ?ｿｽ?ｿｽs?ｿｽﾈゑｿｽ-1?ｿｽ?ｿｽﾔゑｿｽ
+static int refreshTable(int table_val[8][15])
+{
+  uint32_t net_table[8][15];
+  if ((g_buf_len = read(g_sockfd, net_table, 480)) > 0) {
+    int i, j;
+    //?ｿｽS?ｿｽﾄのテ?ｿｽ[?ｿｽu?ｿｽ?ｿｽ?ｿｽﾌ要?ｿｽf?ｿｽ?ｿｽ?ｿｽl?ｿｽb?ｿｽg?ｿｽ?ｿｽ?ｿｽ[?ｿｽN?ｿｽI?ｿｽ[?ｿｽ_?ｿｽ[?ｿｽ?ｿｽ?ｿｽ?ｿｽz?ｿｽX?ｿｽg?ｿｽI?ｿｽ[?ｿｽ_?ｿｽ[?ｿｽﾉ変奇ｿｽ
+    for (i = 0; i < 8; i++)
+      for (j = 0; j < 15; j++)
+        table_val[i][j] = ntohl(net_table[i][j]);
+    table_count++;
+    return 0;
+  } else {
+    return (-1);
+  }
+}
+
+//?ｿｽT?ｿｽ[?ｿｽo?ｿｽﾉテ?ｿｽ[?ｿｽu?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?鰍?ｿｽ?ｿｽ?ｿｽﾖ撰ｿｽ?ｿｽB?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾈゑｿｽ0?ｿｽ@?ｿｽ?ｿｽ?ｿｽs?ｿｽ?ｿｽ-1?ｿｽ?ｿｽﾔゑｿｽ
+static int sendTable(int table_val[8][15])
+{
+  uint32_t net_table[8][15];
+  int i, j;
+  //?ｿｽS?ｿｽﾄのテ?ｿｽ[?ｿｽu?ｿｽ?ｿｽ?ｿｽﾌ要?ｿｽf?ｿｽ?ｿｽ?ｿｽz?ｿｽX?ｿｽg?ｿｽI?ｿｽ[?ｿｽ_?ｿｽ[?ｿｽ?ｿｽ?ｿｽ?ｿｽl?ｿｽb?ｿｽg?ｿｽ?ｿｽ?ｿｽ[?ｿｽN?ｿｽI?ｿｽ[?ｿｽ_?ｿｽ[?ｿｽﾉ変奇ｿｽ
+  for (i = 0; i < 8; i++)
+    for (j = 0; j < 15; j++)
+      net_table[i][j] = htonl(table_val[i][j]);
+  //?ｿｽﾏ奇ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽe?ｿｽ[?ｿｽu?ｿｽ?ｿｽ?ｿｽ?乱M
+  if ((g_buf_len = write(g_sockfd, net_table, 480)) > 0) {
+    return (0);
+  } else {
+    return (-1);
+  }
+}
+
+//?ｿｽ\?ｿｽP?ｿｽb?ｿｽg?ｿｽﾌ設抵ｿｽE?ｿｽﾚ托ｿｽ?ｿｽ?ｿｽ?ｿｽs?ｿｽ?ｿｽ ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ0?ｿｽA?ｿｽ?ｿｽ?ｿｽs?ｿｽ?ｿｽ-1?ｿｽ?ｿｽﾔゑｿｽ
+static int openSocket(const char addr[], uint16_t port_num)
+{
+  //?ｿｽ\?ｿｽP?ｿｽb?ｿｽg?ｿｽﾌ撰ｿｽ?ｿｽ?ｿｽ
+  if ((g_sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    return (-1);
+  }
+
+  /* ?ｿｽ|?ｿｽ[?ｿｽg?ｿｽﾆア?ｿｽh?ｿｽ?ｿｽ?ｿｽX?ｿｽﾌ設抵ｿｽ */
+  bzero((char *)&g_client_addr, sizeof(g_client_addr));
+  g_client_addr.sin_family = PF_INET;
+  g_client_addr.sin_port = htons(port_num);
+  g_client_addr.sin_addr.s_addr = inet_addr(addr);
+
+  //IP?ｿｽA?ｿｽh?ｿｽ?ｿｽ?ｿｽX?ｿｽﾅ指?ｿｽ閧ｳ?ｿｽ?ｿｽﾄゑｿｽ?ｿｽﾈゑｿｽ?ｿｽﾆゑｿｽ?ｿｽA?ｿｽz?ｿｽX?ｿｽg?ｿｽ?ｿｽ?ｿｽﾌ会ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾝゑｿｽ
+  if (g_client_addr.sin_addr.s_addr == 0xffffffff) {
+    struct hostent *host;
+    host = gethostbyname(addr);
+    if (host == NULL) {
+      printf("failed to gethostbyname() : %s.\n", addr);
+      return -1; //?ｿｽz?ｿｽX?ｿｽg?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾉ趣ｿｽ?ｿｽs?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾆゑｿｽ?ｿｽA-1?ｿｽ?ｿｽﾔゑｿｽ
+    }
+    g_client_addr.sin_addr.s_addr =
+        *(unsigned int *)host->h_addr_list[0];
+  }
+
+  /* ?ｿｽT?ｿｽ[?ｿｽo?ｿｽﾉコ?ｿｽl?ｿｽN?ｿｽg?ｿｽ?ｿｽ?ｿｽ?ｿｽ */
+  if (connect(g_sockfd, (struct sockaddr *)&g_client_addr, sizeof(g_client_addr)) == 0) {
+    return 0;
+  }
+  return -1;
+}
+
+//?ｿｽN?ｿｽ?ｿｽ?ｿｽC?ｿｽA?ｿｽ?ｿｽ?ｿｽg?ｿｽﾌ擾ｿｽ?ｿｽ?乱M
+static int sendProfile(const char user[15])
+{
+  int profile[8][15];
+  int i;
+
+  bzero((char *)&profile, sizeof(profile)); //?ｿｽe?ｿｽ[?ｿｽu?ｿｽ?ｿｽ?ｿｽ?ｿｽ0?ｿｽﾅゑｿｽ?ｿｽﾟゑｿｽ
+  profile[0][0] = PROTOCOL_VERSION;         //2007?ｿｽN?ｿｽx?ｿｽﾅゑｿｽ骭ｾ
+  for (i = 0; i < 15; i++)
+    profile[1][i] = (int)user[i]; //?ｿｽ?ｿｽ?ｿｽ?ｿｽﾒ厄ｿｽ?ｿｽ?ｿｽ?ｿｽe?ｿｽ[?ｿｽu?ｿｽ?ｿｽ?ｿｽﾉ格?ｿｽ[
+
+  //?ｿｽ?ｿｽ?ｿｽM
+  if (sendTable(profile) == -1) { //?ｿｽ?ｿｽ?ｿｽs?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽG?ｿｽ?ｿｽ?ｿｽ[?ｿｽ?ｿｽ?ｿｽo?ｿｽﾍゑｿｽ?ｿｽ?ｿｽ~
+    printf("sending profile table was failed.\n");
+    exit(1);
+  }
+
+  //?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽlogging?ｿｽt?ｿｽ?ｿｽ?ｿｽO?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽﾄゑｿｽ?ｿｽ?ｿｽ?ｿｽ?ｿｽA?ｿｽ?ｿｽ?ｿｽb?ｿｽZ?ｿｽ[?ｿｽW?ｿｽﾆテ?ｿｽ[?ｿｽu?ｿｽ?ｿｽ?ｿｽﾌ難ｿｽ?ｿｽe?ｿｽ?ｿｽ?ｿｽo?ｿｽﾍゑｿｽ?ｿｽ?ｿｽ
+  if (g_logging == 1) {
+    printf("sending profile was done successfully.\n");
+    printf("sent profile table was......\n");
+    outputTable(profile);
+  }
+  return 0;
+}
